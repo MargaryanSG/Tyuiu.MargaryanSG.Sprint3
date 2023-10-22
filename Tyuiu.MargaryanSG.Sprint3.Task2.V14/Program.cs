9a13ea189fa9e5a17d076b6d64e0a415210095ca
Tyuiu.MargaryanSG@outlook.com <Tyuiu.MargaryanSG@outlook.com>
2023-10-22 20:30:51 +0500

    Task3

---
+++ Tyuiu.MargaryanSG.Sprint3.Task2.V14/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.MargaryanSG.Sprint3.Task3.V14.Lib;
+
+namespace Tyuiu.MargaryanSG.Sprint3.Task3.V14
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            DataService ds = new DataService();
+
+            string value = "dgt ghedd bed"; char replaceable = 'd'; char replacement = '’';
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("* value = 0.25, startValue = 1, stopValue = 8                             *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            string q = ds.ReplaceCharInString(value, replaceable, replacement);
+
+            Console.WriteLine(q);
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ Tyuiu.MargaryanSG.Sprint3.Task3.V14.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MargaryanSG.Sprint3.Task3.V14.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.MargaryanSG.Sprint3.Task3.V14.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("69a1d09f-6fbb-4885-8fc7-b365bf34ec90")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
